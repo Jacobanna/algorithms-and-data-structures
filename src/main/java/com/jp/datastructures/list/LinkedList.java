9b0c464b39a26f5d02054be6743004762d449a4b
Jacobanna <pieprzyk.jakub@gmail.com>
2019-03-19 15:26:20 +0200

    linked lists - addAtPos() with tests

--- src/main/java/com/jp/datastructures/list/LinkedList.java
+++ src/main/java/com/jp/datastructures/list/LinkedList.java
@@ -39,4 +39,29 @@ public class LinkedList {
         list += temp.getData();
         return list;
     }
+
+    public void addAtPos(int index, Node nodeAtPos) {
+        Node temp = head;
+        int position = 0;
+        if (index == 0) {
+            addFirst(nodeAtPos);
+            return;
+        } else {
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+                position++;
+                if (index == position + 1) {
+                    nodeAtPos.setNext(temp.getNext());
+                    temp.setNext(nodeAtPos);
+                    return;
+                }
+            }
+        }
+        if (index == position + 1) {
+            temp.setNext(nodeAtPos);
+        } else {
+            System.out.println("List has " + (position+1) + " elements, possible indexes to addAtPos are: " + 0 + "-" +
+                    position);
+        }
+    }
 }
--- src/test/java/com/jp/datastructures/list/LinkedListTest.java
+++ src/test/java/com/jp/datastructures/list/LinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class LinkedListTest {
     @Test
-    public void testAddFirst() {
+    public void addFirst() {
         //Given
         Node node1 = new Node(15);
         Node node2 = new Node(33);
@@ -21,7 +21,7 @@ public class LinkedListTest {
     }
 
     @Test
-    public void testAddLast() {
+    public void addLast() {
         //Given
         Node node1 = new Node(25);
         Node node2 = new Node(43);
@@ -32,6 +32,87 @@ public class LinkedListTest {
         linkedList.addLast(node1);
         linkedList.addLast(node2);
         linkedList.addLast(node3);
+        //Then
+        Assert.assertEquals(expectedList, linkedList.traverse());
+    }
+
+    @Test
+    public void addAtPosFirst() {
+        //Given
+        Node node1 = new Node(35);
+        Node node2 = new Node(43);
+        Node node3 = new Node(22);
+        Node node4 = new Node(-4);
+        Node node5 = new Node(7);
+        LinkedList linkedList = new LinkedList();
+        String expectedList = "7 35 43 22 -4";
+        //When
+        linkedList.addLast(node1);
+        linkedList.addLast(node2);
+        linkedList.addLast(node3);
+        linkedList.addLast(node4);
+        linkedList.addAtPos(0, node5);
+        //Then
+        Assert.assertEquals(expectedList, linkedList.traverse());
+    }
+
+    @Test
+    public void addAtPosMiddle() {
+        //Given
+        Node node1 = new Node(35);
+        Node node2 = new Node(43);
+        Node node3 = new Node(22);
+        Node node4 = new Node(-4);
+        Node node5 = new Node(7);
+        LinkedList linkedList = new LinkedList();
+        String expectedList = "35 43 22 7 -4";
+        //When
+        linkedList.addLast(node1);
+        linkedList.addLast(node2);
+        linkedList.addLast(node3);
+        linkedList.addLast(node4);
+        linkedList.addAtPos(3, node5);
+        //Then
+        Assert.assertEquals(expectedList, linkedList.traverse());
+    }
+
+    @Test
+    public void addAtPosLast() {
+        //Given
+        Node node1 = new Node(35);
+        Node node2 = new Node(43);
+        Node node3 = new Node(22);
+        Node node4 = new Node(-4);
+        Node node5 = new Node(7);
+        LinkedList linkedList = new LinkedList();
+        String expectedList = "35 43 22 -4 7";
+        //When
+        linkedList.addLast(node1);
+        linkedList.addLast(node2);
+        linkedList.addLast(node3);
+        linkedList.addLast(node4);
+        linkedList.addAtPos(4, node5);
+        //Then
+        Assert.assertEquals(expectedList, linkedList.traverse());
+    }
+
+    @Test
+    public void addAtPosOutOfBoundaries() {
+        //Given
+        Node node1 = new Node(35);
+        Node node2 = new Node(43);
+        Node node3 = new Node(22);
+        Node node4 = new Node(-4);
+        Node node5 = new Node(7);
+        LinkedList linkedList = new LinkedList();
+        String expectedList = "35 43 22 -4";
+        //When
+        linkedList.addLast(node1);
+        linkedList.addLast(node2);
+        linkedList.addLast(node3);
+        linkedList.addLast(node4);
+        linkedList.addAtPos(-3, node5);
+        linkedList.addAtPos(9, node5);
         //Then
         Assert.assertEquals(expectedList, linkedList.traverse());
     }
