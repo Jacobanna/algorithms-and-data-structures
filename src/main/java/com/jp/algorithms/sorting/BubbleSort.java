00fb11c84f5d7a10e952688fc004315aeeb82101
Jacobanna <pieprzyk.jakub@gmail.com>
2019-03-25 23:46:07 +0100

    Bubble Sort implementation.

---
+++ src/main/java/com/jp/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.jp.algorithms.sorting;
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] array) {
+        int count = 0;
+        int temp;
+        boolean isSwapped = true;
+        while (count < array.length - 1 && isSwapped) {
+            isSwapped = false;
+            for (int i = 0; i < array.length - 1 - count; i++) {
+                if (array[i] > array[i+1]) {
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    isSwapped = true;
+                }
+            }
+            count++;
+        }
+        return array;
+    }
+}
---
+++ src/test/java/com/jp/algorithms/sorting/BubbleSortTest.java
@@ -0,0 +1,49 @@
+package com.jp.algorithms.sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class BubbleSortTest {
+    @Test
+    public void bubbleSortManyValues() {
+        //Given
+        int[] array = new int[7];
+        array[0] = 7;
+        array[1] = 14;
+        array[2] = -3;
+        array[3] = 27;
+        array[4] = 44;
+        array[5] = 31;
+        array[6] = -200;
+        //When
+        array = BubbleSort.bubbleSort(array);
+        int[] expectedArray = {-200, -3, 7, 14, 27, 31, 44};
+        //Then
+        Assert.assertTrue(Arrays.equals(expectedArray, array));
+    }
+
+    @Test
+    public void bubbleSortOneValue() {
+        //Given
+        int[] array = new int[1];
+        array[0] = 7;
+        //When
+        array = BubbleSort.bubbleSort(array);
+        int[] expectedArray = {7};
+        //Then
+        Assert.assertTrue(Arrays.equals(expectedArray, array));
+    }
+
+    @Test
+    public void bubbleSortNoValues() {
+        //Given
+        int[] array = new int[3];
+        //When
+        array = BubbleSort.bubbleSort(array);
+        int[] expectedArray = {0, 0, 0};
+        //Then
+        Assert.assertTrue(Arrays.equals(expectedArray, array));
+    }
+}
